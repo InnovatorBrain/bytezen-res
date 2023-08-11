2802cb0cfa68b43842cd80a7a0041f623aa71e22
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-08-11 12:49:52 +0500

    Header Ok

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.css';
 import images from '../../constants/images';
 
 const Header = () => {
+  useEffect(() => {
+    const handleScrollClick = () => {
+      window.scrollBy(0, 600);
+    };
+
+    const scrollElement = document.getElementById("scroll");
+    if (scrollElement) {
+      scrollElement.addEventListener("click", handleScrollClick);
+    }
+
+    return () => {
+      if (scrollElement) {
+        scrollElement.removeEventListener("click", handleScrollClick);
+      }
+    };
+  }, []);
   return (
     <>
       <div className='app__Header'>
@@ -26,7 +42,16 @@ const Header = () => {
                   <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
                 </svg>
               </button>
+
             </div>
+            <div class="center">
+              <svg width="30" height="100" viewBox="0 0 50 100" id="scroll">
+                <rect x="10" y="5" rx="10" ry="10" width="30" height="50" stroke="var(--color-white)" fill="transparent" stroke-width="3"></rect>
+                <circle cx="25" cy="25" r="6" fill="var(--color-white)" id="circle--shape"></circle>
+              </svg>
+            </div>
+
+
           </div>
 
 
